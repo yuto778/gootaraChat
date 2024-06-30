6b2c78c438fc3cae45d573f4e238effd9e716235
yuto778 <yuto4949@i.softbank.jp>
2024-06-30 20:20:00 +0900

    second commit

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,14 +1,3 @@
-import LoginForm from "./LoginForm";
-
 export default function LoginPage() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
-      <div className="max-w-md w-full space-y-8">
-        <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-          ログイン
-        </h2>
-        <LoginForm />
-      </div>
-    </div>
-  );
+  return <div>ログイン</div>;
 }
